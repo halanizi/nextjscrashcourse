a24f78ef561f2db60be78b71ab8062a464058643
Halanizi <halanizi@gmail.com>
2025-01-14 15:47:34 -0800

    fetching data and type safety category implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,15 @@
 // import Image from "next/image";
 
+import { client } from "@/sanity/lib/client";
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
     const query = (await searchParams).query;
 
-    const posts = [
-        {
-            _createdAt: new Date(),
-            views: 55,
-            author: { _id: 1, name: "Hanna" },
-            _id: 1,
-            description: "This is a description",
-            image: "https://placehold.co/600x400",
-            category: "Robots",
-            title: "We Robots",
-        },
-    ];
+    const posts = await client.fetch(STARTUPS_QUERY);
+
     return (
         <>
             <section className="pink_container">
@@ -32,7 +24,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
             <section className="section_container">
                 <p className="text-30-semibold">{query ? `Search results for "${query}"` : "All Startups"}</p>
 
-                <ul className="mt-7 card_grid">{posts?.length > 0 ? posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />) : <p className="no-results">No startups found.</p>}</ul>
+                <ul className="mt-7 card_grid">{posts?.length > 0 ? posts.map((post: StartupTypeCard) => <StartupCard key={post?._id} post={post} />) : <p className="no-results">No startups found.</p>}</ul>
             </section>
         </>
     );
