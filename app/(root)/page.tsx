9fac1a0f3f6800c0710ba485e0bf1968f072de9b
Halanizi <halanizi@gmail.com>
2025-01-14 10:39:01 -0800

    search form section completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 // import Image from "next/image";
 
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+    const query = (await searchParams).query;
     return (
         <>
             <section className="pink_container">
@@ -8,6 +11,8 @@ export default function Home() {
                     Pitch Your Startup, <br /> Connect with Entrepreneurs
                 </h1>
                 <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+
+                <SearchForm query={query} />
             </section>
         </>
     );
