0662602429e9168283d398600da42e382daec5bf
Halanizi <halanizi@gmail.com>
2025-01-13 17:25:52 -0800

    hero section added, also added shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 import tailwindcssTypography from "@tailwindcss/typography";
-
 const config: Config = {
     darkMode: ["class"],
     content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./sanity/**/*.{js,ts,jsx,tsx,mdx}"],
