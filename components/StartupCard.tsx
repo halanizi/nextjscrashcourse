78d753435895935e9363f3c31bc92ecc522d93db
Halanizi <halanizi@gmail.com>
2025-01-17 13:28:01 -0800

    user profile completed

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,14 +1,14 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "./ui/button";
-
+import { Button } from "@/components/ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
+
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
-    // destructuring for easier code readibility
     const { _createdAt, views, author, title, category, _id, image, description } = post;
 
     return (
@@ -26,20 +26,18 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                     <Link href={`/user/${author?._id}`}>
                         <p className="text-16-medium line-clamp-1">{author?.name}</p>
                     </Link>
-
                     <Link href={`/startup/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
-
                 <Link href={`/user/${author?._id}`}>
-                    <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
+                    <Image src={author?.image!} alt={author?.name!} width={48} height={48} className="rounded-full" />
                 </Link>
             </div>
 
             <Link href={`/startup/${_id}`}>
-                <p className="startup-card-desc">{description}</p>
-                {/* dont use optimization next image html type for now */}
+                <p className="startup-card_desc">{description}</p>
+
                 <img src={image} alt="placeholder" className="startup-card_img" />
             </Link>
 
@@ -54,5 +52,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
     );
 };
+
+export const StartupCardSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn("skeleton", index)}>
+                <Skeleton className="startup-card_skeleton" />
+            </li>
+        ))}
+    </>
+);
 
 export default StartupCard;
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://403146fdcc2c4e3de1c502298be24fc3@o4508660871987200.ingest.us.sentry.io/4508660879720448",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
