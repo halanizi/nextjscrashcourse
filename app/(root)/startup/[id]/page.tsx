a9d32fd9cf52d9991f65efbffcae2414d05de46f
Halanizi <halanizi@gmail.com>
2025-01-18 18:44:34 -0800

    finished course, ready for deployment

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import Link from "next/link";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -11,6 +11,7 @@ import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
@@ -19,9 +20,21 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUP_BY_ID_QUERY, {
-        id,
-    });
+    // this was sequential fetching
+    // const post = await client.fetch(STARTUP_BY_ID_QUERY, {
+    //     id,
+    // });
+
+    // // editor picks is the slug created in sanity referring to a playlist schema slug
+    // const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks" });
+
+    // instead of sequential, let's do parallel fetching to decrease time of querying. they are independent queries so we can run them in parallel
+    const [post, { select: editorPosts }] = await Promise.all([
+        client.fetch(STARTUP_BY_ID_QUERY, { id }),
+        client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+            slug: "editor-picks-new",
+        }),
+    ]);
 
     if (!post) return notFound();
 
@@ -56,7 +69,16 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
                 <hr className="divider" />
 
-                {/* TODO: editor selected startups */}
+                {editorPosts?.length > 0 && (
+                    <div className="max-w-4xl mx-auto">
+                        <p className="text-30-semibold">Editor Picks</p>
+                        <ul className="mt-7 card_grid-sm">
+                            {editorPosts.map((post: StartupTypeCard, i: number) => (
+                                <StartupCard key={i} post={post} />
+                            ))}
+                        </ul>
+                    </div>
+                )}
 
                 <Suspense fallback={<Skeleton className="view_skeleton" />}>
                     <View id={id} />
